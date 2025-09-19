299e93ea008d60b82170dbdcf7aae9720ecd801d
ManasSuple <manassuple163@gmail.com>
2025-09-19 23:31:27 +0530

    Repo Initiated
    First commit

---
+++ my-app/postcss.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
