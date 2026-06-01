289c814f77a9e69f592b0a70950e1b37ae061979
ManasSuple <manassuple163@gmail.com>
2026-06-02 00:42:52 +0530

    UR

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 max-lg:self-center">
                 <Link
-                  href="/Resume-new.pdf"
+                  href="/MANAS-RESUME-UPDATED.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg  font-semibold
                    hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
